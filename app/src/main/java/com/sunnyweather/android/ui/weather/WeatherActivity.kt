30dcefdf6c8967fa927f36c0254a1dcfbd614c61
Tony <tony@gmail.com>
2021-01-31 12:04:48 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/sunnyweather/android/ui/weather/WeatherActivity.kt
@@ -1,13 +1,17 @@
 package com.sunnyweather.android.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sunnyweather.android.R
@@ -32,6 +36,24 @@ class WeatherActivity : AppCompatActivity()
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         window.statusBarColor = Color.TRANSPARENT
         setContentView(R.layout.activity_weather)
+        navBtn.setOnClickListener()
+        {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener
+        {
+            override fun onDrawerStateChanged(newState: Int) {}
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+
+            override fun onDrawerOpened(drawerView: View) {}
+
+            override fun onDrawerClosed(drawerView: View)
+            {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        })
         if(viewModel.locationLng.isEmpty()) viewModel.locationLng = intent.getStringExtra("location_lng") ?: ""
         if(viewModel.locationLat.isEmpty()) viewModel.locationLat = intent.getStringExtra("location_lat") ?: ""
         if(viewModel.placeName.isEmpty()) viewModel.placeName = intent.getStringExtra("place_name") ?: ""
@@ -43,8 +65,20 @@ class WeatherActivity : AppCompatActivity()
                 Toast.makeText(this, "无法成功获取天气信息", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
+            swipeRefresh.isRefreshing = false
         })
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary)
+        refreshWeather()
+        swipeRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
         viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+    }
+
+    fun refreshWeather()
+    {
+        viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
+        swipeRefresh.isRefreshing = true
     }
 
     private fun ShowWeatherInfo(weather:Weather) {
